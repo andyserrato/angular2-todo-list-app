b697d84a295e072cea954974a3eca3e558fd660a
andyserrato <andyserrato@hotmail.com>
2017-07-02 16:27:36 +0200

    My first App using Angular 2 Animations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HowtoComponent } from './howto/howto.component';
+import { ExamplesComponent } from './examples/examples.component';
+import {routes} from "./app.routes";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HowtoComponent,
+    ExamplesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    routes,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
